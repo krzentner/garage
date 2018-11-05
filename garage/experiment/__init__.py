ab0e50e61d555c0889e816eb00f0b5c5e5be89f7
jonashen <jshen100@gmail.com>
2018-11-05 13:47:01 -0800

    Move garage.misc.instrument to garage.experiment

---
+++ garage/experiment/__init__.py
@@ -0,0 +1,14 @@
+"""Experiment functions."""
+from garage.experiment.experiment import concretize
+from garage.experiment.experiment import run_experiment
+from garage.experiment.experiment import to_local_command
+from garage.experiment.experiment import variant
+from garage.experiment.experiment import VariantGenerator
+
+__all__ = [
+    "concretize",
+    "run_experiment",
+    "to_local_command",
+    "variant",
+    "VariantGenerator",
+]
